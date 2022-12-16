872429918d8ea8c0181f52f57c31854919e9b52f
Swapnilveer <veerswapnil77@gmail.com>
2022-12-16 16:33:12 +0530

    Basic Commit
    Hi all,
    I have created project on github.com go ahead and clone it. and start with your respective task.

---
+++ DemoProject/src/pageLayer/TestBase.java
@@ -0,0 +1,9 @@
+package pageLayer;
+
+public class TestBase {
+
+	public static void main(String[] args) {
+		System.out.println("TestBase");
+		System.out.println("Added dependencies in pom.xml");
+	}
+}
